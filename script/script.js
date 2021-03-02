c7725f6f2bc48a823f323086ffcaa497fbe23593
aaoshchepkov <aa_oshchepkov@mail.ru>
2021-03-02 14:26:55 +0700

    урок 24

--- script/script.js
+++ script/script.js
@@ -325,10 +325,8 @@ window.addEventListener("DOMContentLoaded", function () {
       const regularValid = () => {
         target.value = target.value.replace(/ +/g, ' ');
         target.value = target.value.replace(/-+/g, '-');
-        target.value = target.value.replace(/^-/g, '');
-        target.value = target.value.replace(/-$/g, '');
-        target.value = target.value.replace(/^ /g, '');
-        target.value = target.value.replace(/ $/g, '');
+        target.value = target.value.replace(/^-|-$/g, '');
+        target.value = target.value.trim();
       };
 
       if (target.matches('#form2-message')) {
@@ -378,4 +376,50 @@ window.addEventListener("DOMContentLoaded", function () {
 
   validate();
 
+  // Калькулятор
+  const calc = (price = 100) => {
+
+    const calcBlock = document.querySelector('.calc-block'),
+          calcType = document.querySelector('.calc-type'),
+          calcSquare = document.querySelector('.calc-square'),
+          calcCount = document.querySelector('.calc-count'),
+          calcDay = document.querySelector('.calc-day'),
+          totalValue = document.getElementById('total');
+
+    const constSum = () =>{
+      let total = 0;
+      let countValue = 1;
+      let dayValue = 1;
+      const typeValue = calcType.options[calcType.selectedIndex].value;
+      const squareValue = +calcSquare.value;
+      
+      if (calcCount.value > 1) {
+        countValue+= (calcCount.value -1) /10;
+      }
+
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue*= 2;
+
+      } else if(calcDay.value && calcDay.value < 10) {
+        dayValue*= 1.5;
+      }
+
+      if(typeValue && squareValue){
+        total = price * typeValue * squareValue * countValue * dayValue;
+      }
+      totalValue.textContent = total;
+    };
+      calcBlock.addEventListener('change', (event) => {
+      const target = event.target;
+      if(target.matches('.calc-day') || target.matches('.calc-square') || 
+      target.matches('.calc-type') || target.matches('.calc-count')){
+        constSum();
+
+      }
+
+    });      
+
+
+  };
+   calc(100);
 });
